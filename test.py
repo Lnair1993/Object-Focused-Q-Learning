fe39c5ba43830bf701af7d70760a6d690171ff3c
Lakshmi Nair <lnair3@gatech.edu>
2017-08-16 23:15:31 -0400

    testing

---
+++ test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 11 11:38:32 2017
+
+@author: Lakshmi
+"""
+
+from mdp import mdp
+
+from object_class import _object
+from object_class import object_class
+
+from OfQ import OfQ_Learning
+
+actlist = ["Up", "Down", "Left", "Right", "Wait"]
+grid = [[-0.04, -0.04, -0.04, -0.04],
+        [-0.04, -0.04, -0.04, -0.04],
+        [-0.04, -0.04, -0.04, -0.04],
+        [-0.04, -0.04, -0.04, -0.04],
+        [-0.04, -0.04, -0.04, -0.04]]
+                                   
+terminals = [(2,0),(1,0),(3,1)]
+agent_start = (1,1)
+gamma = 0.99
+
+#Define these based on the game
+enemies = object_class("Enemies") #Enemies class
+gold = object_class("Gold") #Final gold class
+
+o1 = _object("Bullet1", enemies, (2,0), -100)
+o2 = _object("Bullet2", enemies, (3,0), -100)
+o3 = _object("Gold1", gold, (4,1), 100)
+
+#Create object array
+objects = [o1, o2, o3]
+classes = [enemies, gold]
+
+Normandy_mdp = mdp(grid, terminals, actlist, objects, agent_start, gamma)
+
+o_learner = OfQ_Learning(Normandy_mdp, classes, 50, 10)
+o_learner.OFQrun()
+
+#o_learner = OfQ_RT_Learning(Normandy_mdp, classes, 300, 100)
+#o_learner.OFQ_RTrun()
